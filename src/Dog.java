49fcfd5847fb5176f524e5e4b33801766a682786
MAbdulov <123Dd123#>
2021-06-18 16:18:11 +0600

    init

---
+++ src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog {
+    String color;
+    int age;
+
+    public Dog(String color, int age) {
+        this.color = color;
+        this.age = age;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog puppy = new Dog("black", 1);
+    }
+}
